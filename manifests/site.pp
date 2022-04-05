7df687f45d6219d027ad0bf731dae6178974cfd4
shyamjeekumar010797 <35343786+shyamjeekumar010797@users.noreply.github.com>
2022-04-05 14:51:14 +0530

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,3 +8,12 @@ file {'/root/README':
 node testcentos.w1j0tr3af22urntgjrhnaj25nb.cx.internal.cloudapp.net{
 include role::master_server
 }
+
+node /^web/ {
+include role::app_server
+}
+
+
+node /^db/ {
+include role::db_server
+}
